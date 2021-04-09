6fdb224da16787bf3adcf8a9ad47bdad31b6a632
Diego Raul <devup2332@gmail.com>
2021-04-08 23:50:04 -0500

    api-uri changed 2

--- src/environments.tsx
+++ src/environments.tsx
@@ -1,5 +1,5 @@
 export const environments = {
-  api_uri: "http://localhost:8080/api",
+  api_uri: "https://m-gallery.xyz/api",
   email_pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
   number_pattern: /^[0-9]+$/,
 };
